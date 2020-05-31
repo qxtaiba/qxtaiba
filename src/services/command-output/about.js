f2b11ac0e8215fee69cb7bb6d71de9532cd20296
qxtaiba <qan205@nyu.edu>
2020-06-01 02:33:01 +0400

    added language info

--- src/services/command-output/about.js
+++ src/services/command-output/about.js
@@ -4,6 +4,10 @@ export default [
     '>   D.O.B:        20/01/1999',
     '>   Location:     <color="yellow">Liverpool, United Kingdom</color>',
     ' ',
+    '>   English       []-<color="pct-8">#################################################</color>-[]',
+    '>   Arabic        []-<color="pct-8">#################################################</color>-[]',
+    '>   Spanish       []-<color="pct-4">############################</color>-[]',
+    ' ',
     '>   Education:    <color="yellow">Electrical</color> and <color="yellow">Software Engineer</color> studying at <color="yellow">NYU</color> with a keen interest in <color="yellow">Machine Learning</color>.',
     ' ',
  ]
