5e7c3542238bc3b738b28c90e806c9a8601ef213
qxtaiba <qan205@nyu.edu>
2020-06-01 02:29:18 +0400

    changed prompt

--- src/App.vue
+++ src/App.vue
@@ -8,11 +8,11 @@
     >
         <div class="screen flex-col">
             <ul v-if="history.length" class="flex-col">
-                <output-row v-for="entry in history" :key="entry.id" :entryText="entry.text" promptText="anon@ronniepyne.com" :entryType="entry.type"></output-row>
+                <output-row v-for="entry in history" :key="entry.id" :entryText="entry.text" promptText="anon@qutaiba.com" :entryType="entry.type"></output-row>
             </ul>
 
             <div id="prompt" class="flex-row-center input-row">
-                <span class="prompt-icon">anon@ronniepyne.com&nbsp;$</span> 
+                <span class="prompt-icon">anon@qxtaiba.com&nbsp;$</span> 
                 <input
                     ref="input" 
                     v-model="userInput" type="text"
